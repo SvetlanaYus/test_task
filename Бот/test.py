f3f948c1f6f10f245616f877c64a450f0357c25a
SvetlanaYus <yushkova.lana1998@gmail.com>
2022-07-26 20:11:45 +0500

    feat: тестовое задание - бот

--- Бот/test.py
+++ Бот/test.py
@@ -2,7 +2,7 @@ import emoji
 import uuid
 import datetime
 
-my_id = uuid.uuid1() # or uuid.uuid4()
+
 id = []
 # print(my_id)
 # print(emoji.demojize('😬'))
@@ -16,6 +16,7 @@ ANGRY = [':angry_face_with_horns:', ':grimacing_face:']
 
 a = 1
 while a == 1:
+    my_id = uuid.uuid1() # or uuid.uuid4()
     message = input()
     message_dem = emoji.demojize(message)
     time = str(datetime.datetime.now())[11:]
@@ -35,8 +36,10 @@ while a == 1:
                         print('Хорошо, когда настреоние всегда на высоте!')
                     elif second_query in BAD:
                         print('Переменчивое у Вас настроение')
-                    else:
+                    elif second_query in ANGRY:
                         print('Думаю, Вам стоит помедитировать, вся злость испарится')
+                    else:
+                        print('Совсем не понимаю людей')
         except second_query is None:
             print('Конец')
 
@@ -44,9 +47,27 @@ while a == 1:
         print('Привет! Видимо, у Вас что-то стряслось. Не грустите')
     elif message_dem in ANGRY:
         print('Ну привет, человек не в настроении')
+        try:
+            second_query = input()
+            second_query = emoji.demojize(second_query)
+            time_2 = str(datetime.datetime.now())[11:]
+            time_2 = time_2.split(':')
+            for i in range(len(time)):
+                if int(time_2[1]) - int(time[1]) <= 1:
+                    if second_query in GOOD:
+                        print('У людей всегда настроение поднимается, когда со мной поговорят')
+                    elif second_query in BAD:
+                        print('Эх, веселее Вы не стали..')
+                    elif second_query in ANGRY:
+                        a = 'Медитация - вот выход'
+                        print(a)
+                    else:
+                        print('Совесем перестал людей понимать')
+        except second_query is None:
+            print('Конец')
     else:
-        a = 0
-        print('Совсем я разучился понимать человеческий язык...')
+        print('Не понимаю Вас совсем')
+        my_id = uuid.uuid1() # or uuid.uuid4()
 
        
         
